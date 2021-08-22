e94c7ca66d09fd3cce642f29f86845c24e5cd726
tadona <conanbossun@gmail.com>
2021-08-22 11:18:15 +0900

    feat laravel-Vue

---
+++ backend/resources/js/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+ <template>
+     <div class="container-fluid bg-dark mb-3">
+         <div class="container">
+             <nav class="navbar navbar-dark">
+                 <router-link v-bind:to="{name: 'task.index'}">
+                 <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
+                 </router-link>
+                 <div>
+                     <router-link v-bind:to="{name: 'task.list'}">
+                        <button class="btn btn-success">List</button>
+                     </router-link>
+                     <router-link v-bind:to="{name: 'task.create'}">
+                        <button class="btn btn-success">ADD</button>
+                       </router-link> 
+                 </div>
+             </nav>
+         </div>
+     </div>
+ </template>
+ 
+ <script>
+     export default {}
+ </script>
